5af33cbe91dcebdc083ec41ddee4ae43ef08d1c2
Erivando Sena <erivandoramos@bol.com.br>
2017-10-02 01:05:37 -0300

    Conteúdo referente a tudo que foi desenvolvido em 30/09/2017, seguindo os requisitos iniciais do projeto.

---
+++ app/src/main/java/br/com/erivando/gestanteautocuidadopa/FragmentMenuActivity.java
@@ -0,0 +1,22 @@
+package br.com.erivando.gestanteautocuidadopa;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentMenuActivity extends Fragment {
+
+    public FragmentMenuActivity() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_menu, container, false);
+        return  rootView;
+    }
+
+}
