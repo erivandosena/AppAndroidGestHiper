32186fa1746b2d09ee0bd83010865d7c636e783b
Erivando Sena <erivandoramos@bol.com.br>
2017-10-14 17:31:22 -0300

    Inclusão de ilustrações ns telas das opções do menu. Iniciado o desenvolvimento do diario da pa. Melhorias na cor de fundo das telas.

--- app/src/main/java/br/com/erivando/gestanteautocuidadopa/fragment/AfericaoPAFragment.java
+++ app/src/main/java/br/com/erivando/gestanteautocuidadopa/fragment/AfericaoPAFragment.java
@@ -4,66 +4,86 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-import com.bluejamesbond.text.DocumentView;
-
 import br.com.erivando.gestanteautocuidadopa.R;
 
 /**
- * Projeto: gestante-autocuidado-da-pa
+ * Projeto: GestanteAutocuidadoPA
  * Criado por Erivando Sena
- * Data/Hora: 06 de Outubro de 2017 as 10:57h
+ * Data/Hora: 13 de Outubro de 2017 as 15:20h
  * Local: Fortaleza/CE
  * E-mail: erivandoramos@bol.com.br
  */
 
-public class OpcaoSeteFragment extends Fragment {
+public class AfericaoPAFragment extends Fragment {
 
     private FragmentManager fragmentManager;
     private FragmentTransaction fragmentTransaction;
     private Fragment fragment;
     private Class fragmentClass;
 
-    public OpcaoSeteFragment() {
+    public AfericaoPAFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_opcao_sete, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_afericao_pa, container, false);
 
         fragmentManager = getFragmentManager();
 
-        DocumentView documentViewSete = (DocumentView) rootView.findViewById(R.id.txt_opcao_sete);
-        documentViewSete.setText(Html.fromHtml(String.valueOf(documentViewSete.getText())));
-
-        Button btRegistrarAfericaoPa = (Button) rootView.findViewById(R.id.bt_registro_afericao_pa);
-        btRegistrarAfericaoPa.setOnClickListener(new View.OnClickListener() {
+        Button btOpcaoSeis = (Button) rootView.findViewById(R.id.bt_como_aferir_pressao);
+        btOpcaoSeis.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentClass = AfericaoPAFragment.class;
+                fragmentClass = OpcaoSeisFragment.class;
                 try {
                     fragment = (Fragment) fragmentClass.newInstance();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-                fragmentTransaction.replace(R.id.flContent, fragment);
-                fragmentTransaction.commit();
+                fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
             }
         });
 
-        ImageButton btAnteriorOpcaoSeis = (ImageButton) rootView.findViewById(R.id.bt_ant_opcao_seis);
-        btAnteriorOpcaoSeis.setOnClickListener(new View.OnClickListener() {
+        Button btOpcaoCinco = (Button) rootView.findViewById(R.id.bt_observar_afericao);
+        btOpcaoCinco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentClass = OpcaoCincoFragment.class;
+                try {
+                    fragment = (Fragment) fragmentClass.newInstance();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+            }
+        });
+
+        Button btOpcaoTres = (Button) rootView.findViewById(R.id.bt_deve_saber);
+        btOpcaoTres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentClass = OpcaoTresFragment.class;
+                try {
+                    fragment = (Fragment) fragmentClass.newInstance();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+            }
+        });
+
+        ImageButton btAnteriorOpcaoSete = (ImageButton) rootView.findViewById(R.id.bt_ant_opcao_sete);
+        btAnteriorOpcaoSete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentClass = OpcaoSeisFragment.class;
+                fragmentClass = OpcaoSeteFragment.class;
                 try {
                     fragment = (Fragment) fragmentClass.newInstance();
                 } catch (Exception e) {
@@ -89,25 +109,7 @@ public class OpcaoSeteFragment extends Fragment {
                 fragmentTransaction.commit();
             }
         });
-		
-        ImageButton btProximoOpcaoOito = (ImageButton) rootView.findViewById(R.id.bt_prox_opcao_oito);
-        btProximoOpcaoOito.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentClass = OpcaoOitoFragment.class;
-                try {
-                    fragment = (Fragment) fragmentClass.newInstance();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                fragmentTransaction.replace(R.id.flContent, fragment);
-                fragmentTransaction.commit();
-            }
-        });
 
         return rootView;
     }
-
 }
